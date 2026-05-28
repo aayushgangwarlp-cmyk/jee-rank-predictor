75f33c144ccb94a5f02b7d306ef64ab0a109f23d
Ayush Gangwar <aayushgangwar.lp@gmail.com>
2026-05-28 22:21:19 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+
+  const [percentile, setPercentile] = useState("");
+  const [rank, setRank] = useState("");
+
+  const calculateRank = () => {
+
+    const predictedRank =
+      Math.round(((100 - percentile) / 100) * 1000000);
+
+    setRank(predictedRank);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+
+      <h1>JEE Rank Predictor</h1>
+
+      <input
+        type="number"
+        placeholder="Enter Your Percentile"
+        value={percentile}
+        onChange={(e) => setPercentile(e.target.value)}
+      />
+
+      <button onClick={calculateRank}>
+        Predict Rank
+      </button>
+
+      <h2>
+        Predicted Rank: {rank}
+      </h2>
+
     </div>
   );
 }
